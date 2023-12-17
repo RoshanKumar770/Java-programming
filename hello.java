6736e55a66acb45abba94a54b14cadef3343c984
RoshanKumar770 <roshankumar81624@gmail.com>
2023-12-17 08:48:53 +0530

    added feature1

--- hello.java
+++ hello.java
@@ -1,3 +1,3 @@
 import java.util*;
-public static void main(string args[])
-System.out.println("This is feature1 and feature1");
+public static void main(string[]args)
+System.out.println("This is feature1");
